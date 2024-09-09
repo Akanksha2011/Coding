b0d1d9e28b15cff05c29d764a82ac159e5d4fc41
Akanksha Pandey <96917420+Akanksha2011@users.noreply.github.com>
2024-09-09 19:21:49 +0530

    Time: 16 ms (19.42%), Space: 44.8 MB (58.63%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,20 @@
 class Solution {
-    public boolean canConstruct(String ransom, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<magazine.length(); i++)
+    public boolean canConstruct(String ransom, String mag) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<mag.length(); i++)
         {
-            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+            map.put(mag.charAt(i),map.getOrDefault(mag.charAt(i),0)+1);
         }
-        for(int i=0; i<ransom.length(); i++)
-        {
-           if(map.containsKey(ransom.charAt(i)))
-           {
-               map.put(ransom.charAt(i), map.get(ransom.charAt(i)) - 1);
-                if (map.get(ransom.charAt(i)) == 0) {
-                    map.remove(ransom.charAt(i));
-           }
-           }
-           else
-           return false;
+        for(int i=0; i<ransom.length();i++)
+        {   
+            if(!map.containsKey(ransom.charAt(i)))
+            return false;
+            else
+            {
+                map.put(ransom.charAt(i),map.get(ransom.charAt(i))-1);
+                if(map.get(ransom.charAt(i)) == 0)
+                map.remove(ransom.charAt(i));
+            }
         }
         return true;
     }
